4ed20ff9842b54bb781a48e512cf9e2b02be1062
Diego-Diaz797 <ddiaz11@elon.edu>
2025-04-15 15:58:47 -0400

    making the menu

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        //work
         System.out.println("Hello World!");
         System.out.println("IT FUCKING WORKS");
     }
