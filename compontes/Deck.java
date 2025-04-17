b899156929ee8d55bf4bbaa31394f6b0b64bd711
Diego-Diaz797 <ddiaz11@elon.edu>
2025-04-17 14:57:49 -0400

    making the test compontes

---
+++ compontes/Deck.java
@@ -0,0 +1,7 @@
+package compontes;
+
+public class Deck {
+    public void test(){
+        System.out.println("hi");
+    }
+}
--- test.java
+++ test.java
@@ -1,14 +1,20 @@
 //imports
 import java.util.Scanner;
 
+//importing classes
+import compontes.Deck;
+
 public class test {
-    //Scanner
-    Scanner testScan = new Scanner(System.in);
-
-    //testing imports
-
     //testing zone
     public static void main(String[] args) {
+        //Scanner
+        Scanner testScan = new Scanner(System.in);
+
+        //testing imports
+        Deck testDeck = new Deck();
+
         System.out.println("Testing zone");
+        testDeck.test();
+
     }
 }
