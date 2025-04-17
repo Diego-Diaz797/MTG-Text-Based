55af60d9b201e5a4f0201af2c83b22834a92a9eb
Diego-Diaz797 <ddiaz11@elon.edu>
2025-04-17 12:30:21 -0400

    making the test compontes

--- test.java
+++ test.java
@@ -1,4 +1,14 @@
 //imports
+import java.util.Scanner;
 
 public class test {
+    //Scanner
+    Scanner testScan = new Scanner(System.in);
+
+    //testing imports
+
+    //testing zone
+    public static void main(String[] args) {
+        System.out.println("Testing zone");
+    }
 }
